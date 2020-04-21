f3aa80245a684efae4244162c3dbbd706d7dda6a
Aleksandr Fomin <RezviyBelorus@gmail.com>
2020-04-21 21:19:27 +0300

    Update Inch.java

--- src/qa05/lessons_homework/lesson_2/task_5/Inch.java
+++ src/qa05/lessons_homework/lesson_2/task_5/Inch.java
@@ -3,6 +3,7 @@ package lessons_homework.lesson_2.task_5;
 public class Inch {
     public static void main(String[] args) {
         double centimeters = 2.54;
+        // магическое число 20 лучше в отдельную переменную
         for (int i = 1; i <= 20; i++) {
             System.out.println(i + " inches contains " + centimeters * i + " cm");
         }
