b92968375c129bd59d8753acd2bb782d47b3480b
Aleksandr Fomin <RezviyBelorus@gmail.com>
2020-04-23 21:45:55 +0300

    Update Task_4.java

--- src/qa05/lessons_homework/lesson_3/task_4/Task_4.java
+++ src/qa05/lessons_homework/lesson_3/task_4/Task_4.java
@@ -2,6 +2,7 @@ package lessons_homework.lesson_3.task_4;
 
 import java.util.Arrays;
 
+// +
 public class Task_4 {
     public static void main(String[] args) {
         int multiplicationTable[][] = new int[9][9];
