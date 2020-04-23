acfb9913fabe8d78b3875e526abb5b27b56829da
Aleksandr Fomin <RezviyBelorus@gmail.com>
2020-04-23 21:44:28 +0300

    Update Task_2.java

--- src/qa05/lessons_homework/lesson_3/task_2/Task_2.java
+++ src/qa05/lessons_homework/lesson_3/task_2/Task_2.java
@@ -1,5 +1,6 @@
 package lessons_homework.lesson_3.task_2;
 
+// старайся выносить части кода в отдельные методы
 public class Task_2 {
     public static void main(String[] args) {
         int[] array = new int[10];
@@ -11,7 +12,8 @@ public class Task_2 {
         for (int i = 0; i < array.length; i++) {
             averageOfSum += array[i];
         }
-        averageOfSum = averageOfSum / array.length;
+        
+        averageOfSum = averageOfSum / array.length; // а если деление будет с остатком? получишь некорректное среднее
         System.out.println("Average = " + averageOfSum);
     }
 }
