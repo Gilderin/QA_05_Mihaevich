a45cb7c3c9a0a3f1d272337a8efa878357ed3657
Gilderin <BillTestGroup@gmail.com>
2020-04-23 13:37:55 +0300

    Homework.Lesson 3

---
+++ src/qa05/lessons_homework/lesson_3/task_7/Task_7.java
@@ -0,0 +1,45 @@
+package lessons_homework.lesson_3.task_7;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Task_7 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input array size");
+        int arraySize = scanner.nextInt();
+        int[] arrayAscending = new int[arraySize];
+        int[] arrayDescending = new int[arraySize];
+        for (int i = 0; i < arrayAscending.length; i++) {
+            arrayAscending[i] = (int) (Math.random() * 100);
+            arrayDescending[i] = arrayAscending[i];
+        }
+        System.out.println("Default array: "+Arrays.toString(arrayAscending));
+        sort(arrayAscending, arrayDescending);
+        System.out.println("Ascending sort: "+Arrays.toString(arrayAscending));
+        System.out.println("Descending sort: "+Arrays.toString(arrayDescending));
+    }
+
+    public static void sort(int[] arrayA, int[] arrayD) {
+        boolean needSort = true;
+        while (needSort) {
+            needSort = false;
+            for (int i = 1; i < arrayA.length; i++) {
+                if (arrayA[i] < arrayA[i - 1]) {
+                    sort(arrayA, i - 1, i);
+                    needSort = true;
+                }
+                if (arrayD[i] > arrayD[i - 1]) {
+                    sort(arrayD, i - 1, i);
+                    needSort = true;
+                }
+            }
+        }
+    }
+
+    public static void sort(int[] array, int left, int right) {
+        int tmpAscending = array[left];
+        array[left] = array[right];
+        array[right] = tmpAscending;
+    }
+}
---
+++ src/qa05/lessons_homework/lesson_3/task_8/Task_8.java
@@ -0,0 +1,114 @@
+package lessons_homework.lesson_3.task_8;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Task_8 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean wrongSize = true;
+        int sizeButterfly = 0;
+        while (wrongSize) {
+            System.out.println("Please input size of butterfly(3-18):");
+            sizeButterfly = scanner.nextInt();
+            if (sizeButterfly < 3 | sizeButterfly > 18) {
+                System.out.println("wrong size");
+            } else {
+                wrongSize = false;
+            }
+        }
+        String[] leftWing = new String[sizeButterfly];
+        String[] rightWing = new String[sizeButterfly];
+        String butterfly = "";
+        String spaces = "";
+        if (sizeButterfly % 2 == 0) {
+            sizeEven(leftWing, rightWing, butterfly, sizeButterfly, spaces);
+        } else {
+            sizeOdd(leftWing, rightWing, butterfly, sizeButterfly, spaces);
+        }
+        for (int i = 0; i < leftWing.length; i++) {
+            System.out.println(leftWing[i] + rightWing[i]);
+        }
+    }
+
+    public static void sizeEven(String[] leftWing, String[] rightWing, String butterfly, int sizeButterfly, String spaces) {
+        for (int i = 1; i <= leftWing.length / 2; i++) {
+            butterfly += String.valueOf(i);
+            for (int j = 0; j <= leftWing.length / 2; j++) {
+                if (leftWing[j] == null) {
+                    leftWing[j] = butterfly;
+                    break;
+                }
+            }
+        }
+        for (int i = leftWing.length / 2; i < leftWing.length; i++) {
+            if (i == leftWing.length / 2) {
+                leftWing[i] = butterfly;
+            } else {
+                butterfly = butterfly.substring(0, butterfly.length() - 1);
+                leftWing[i] = butterfly;
+            }
+        }
+        for (int i = 0; i < sizeButterfly - 2; i++) {
+            spaces += " ";
+        }
+        butterfly = "";
+        for (int i = 1; i <= rightWing.length / 2; i++) {
+            butterfly += String.valueOf(i);
+            for (int j = 0; j <= rightWing.length / 2; j++) {
+                if (rightWing[j] == null) {
+                    rightWing[j] = spaces + new StringBuffer(butterfly).reverse().toString();
+                    if (rightWing.length / 2 != i)
+                        spaces = spaces.substring(2);
+                    break;
+                }
+            }
+        }
+        for (int i = rightWing.length / 2; i < rightWing.length; i++) {
+            if (i == rightWing.length / 2) {
+                rightWing[i] = new StringBuffer(butterfly).reverse().toString();
+            } else {
+                spaces += "  ";
+                butterfly = butterfly.substring(0, butterfly.length() - 1);
+                rightWing[i] = spaces + new StringBuffer(butterfly).reverse().toString();
+            }
+        }
+    }
+
+    public static void sizeOdd(String[] leftWing, String[] rightWing, String butterfly, int sizeButterfly, String spaces) {
+
+        for (int i = 1; i <= leftWing.length / 2 + 1; i++) {
+            butterfly += String.valueOf(i);
+            for (int j = 0; j <= leftWing.length / 2 + 1; j++) {
+                if (leftWing[j] == null) {
+                    leftWing[j] = butterfly;
+                    break;
+                }
+            }
+        }
+        for (int i = leftWing.length / 2 + 1; i < leftWing.length; i++) {
+            butterfly = butterfly.substring(0, butterfly.length() - 1);
+            leftWing[i] = butterfly;
+        }
+        for (int i = 0; i < sizeButterfly - 1; i++) {
+            spaces += " ";
+        }
+        butterfly = "";
+        for (int i = 1; i <= rightWing.length / 2 + 1; i++) {
+            butterfly += String.valueOf(i);
+            for (int j = 0; j <= rightWing.length / 2 + 1; j++) {
+                if (rightWing[j] == null) {
+                    rightWing[j] = spaces + new StringBuffer(butterfly).reverse().toString();
+                    if (rightWing.length / 2 != j)
+                        spaces = spaces.substring(2);
+                    break;
+                }
+            }
+        }
+        for (int i = rightWing.length / 2 + 1; i < rightWing.length; i++) {
+            spaces += "  ";
+            butterfly = butterfly.substring(0, butterfly.length() - 1);
+            rightWing[i] = spaces + new StringBuffer(butterfly).reverse().toString();
+        }
+    }
+}
