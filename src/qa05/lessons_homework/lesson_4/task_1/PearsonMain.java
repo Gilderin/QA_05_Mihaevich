113c2de7e207832db0b8cfa06c7412945569cb01
Aleksandr Fomin <RezviyBelorus@gmail.com>
2020-04-28 19:34:54 +0300

    Update PearsonMain.java

--- src/qa05/lessons_homework/lesson_4/task_1/PearsonMain.java
+++ src/qa05/lessons_homework/lesson_4/task_1/PearsonMain.java
@@ -2,7 +2,7 @@ package lessons_homework.lesson_4.task_1;
 
 public class PearsonMain {
     public static void main(String[] args) {
-        Pearson pearson = new Pearson();
+        Pearson pearson = new Pearson(); //Person
         System.out.println(pearson.show());
     }
 }
