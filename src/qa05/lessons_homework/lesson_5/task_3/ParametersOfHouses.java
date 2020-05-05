1ecd39e5c0f885e27bd09e622ec805455a597c6a
Aleksandr Fomin <RezviyBelorus@gmail.com>
2020-05-05 20:50:07 +0300

    Update ParametersOfHouses.java

--- src/qa05/lessons_homework/lesson_5/task_3/ParametersOfHouses.java
+++ src/qa05/lessons_homework/lesson_5/task_3/ParametersOfHouses.java
@@ -1,5 +1,8 @@
 package lessons_homework.lesson_5.task_3;
 
+/*
+это не совсем параметры дома, думаю лучше переименовать в House
+*/
 public interface ParametersOfHouses {
 
     void countOfFloors();
