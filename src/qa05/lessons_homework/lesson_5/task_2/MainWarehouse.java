4c741f1ae972941a6c5b10958e4df0e85f96c41a
Aleksandr Fomin <RezviyBelorus@gmail.com>
2020-05-05 20:49:02 +0300

    Update MainWarehouse.java

--- src/qa05/lessons_homework/lesson_5/task_2/MainWarehouse.java
+++ src/qa05/lessons_homework/lesson_5/task_2/MainWarehouse.java
@@ -4,12 +4,16 @@ import java.util.Arrays;
 
 public class MainWarehouse {
     public static void main(String[] args) {
-        Integer[] arrayOne = {1, 2, 1};
+        Integer[] arrayOne = {1, 2, 1}; // так себе название переменной
         Warehouse warehouse = new Warehouse(arrayOne);
         arrayOne[0] = 666;
         System.out.println(warehouse);
         warehouse.next();
         Integer[] newArray = warehouse.getArray();
+        /* а теперь след строкой
+        newArray[0] = 333;
+        проверяй
+        */
         System.out.println(Arrays.toString(newArray));
     }
 }
