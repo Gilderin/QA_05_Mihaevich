63ab501ad7c10fec30ca7fcfaa69b96c037c8b73
Aleksandr Fomin <RezviyBelorus@gmail.com>
2020-05-07 20:23:40 +0300

    Update Task_1.java

--- src/qa05/lessons_homework/lesson_6/task_1/Task_1.java
+++ src/qa05/lessons_homework/lesson_6/task_1/Task_1.java
@@ -2,6 +2,7 @@ package lessons_homework.lesson_6.task_1;
 
 import java.util.Arrays;
 
+// форматируй код
 public class Task_1 {
     public static void main(String[] args) {
         int[] array={1,2,3,5};
