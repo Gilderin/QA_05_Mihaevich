1b94c94005e08ad4f1e969c4b15ac4379bb0e5c7
Aleksandr Fomin <RezviyBelorus@gmail.com>
2020-05-09 12:30:07 +0300

    Update MainTree.java

--- src/qa05/lessons_homework/lesson_6/task_3/MainTree.java
+++ src/qa05/lessons_homework/lesson_6/task_3/MainTree.java
@@ -2,6 +2,6 @@ package lessons_homework.lesson_6.task_3;
 
 public class MainTree {
     public static void main(String[] args) {
-
+// еще должно быть вроде 2 типа обезьян и менеджер обезьян
     }
 }
