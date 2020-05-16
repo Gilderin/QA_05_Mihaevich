98926324ed512008b5fc00d3caa9e2a48a019f7c
Aleksandr Fomin <RezviyBelorus@gmail.com>
2020-05-16 11:41:29 +0300

    Update Task_2.java

--- src/qa05/lessons_homework/lesson_8/task_2/Task_2.java
+++ src/qa05/lessons_homework/lesson_8/task_2/Task_2.java
@@ -7,6 +7,7 @@ import java.util.Iterator;
 / форматируй код
 public class Task_2 {
     public static void main(String[] args) {
+    / создание и заполнение в отдельный метод
         ArrayList<Integer> elements=new ArrayList<>();
         for (int i = 1; i <=100 ; i++) {
             elements.add(0,i);
