30a3fb3304adb3ce45785c2f30ff30226c1756b3
Aleksandr Fomin <RezviyBelorus@gmail.com>
2020-05-16 11:39:37 +0300

    Update Task_1.java

--- src/qa05/lessons_homework/lesson_8/task_1/Task_1.java
+++ src/qa05/lessons_homework/lesson_8/task_1/Task_1.java
@@ -4,15 +4,20 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+// форматируй код
 public class Task_1 {
     public static void main(String[] args) {
         List<String> inviteList=new ArrayList<>();
         inviteList.add("Nikolay");
         inviteList.add("Alex");
         inviteList.add("Lida");
+       
+        / в отдельный метод
         Scanner scaner=new Scanner(System.in);
         System.out.println("Введите свое имя:");
         String name=scaner.next();
+      
+        / в отдельный метод
         if(inviteList.contains(name)){
             System.out.println("Вы приглашены на вечеринку. Проходите");
         }else{
